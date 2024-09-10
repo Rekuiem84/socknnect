301e166532c501c226877bb205d5a494cb9379cc
Rekuiem84 <morinijoshua@gmail.com>
2024-09-11 00:24:12 +0200

    commit

--- include/header.php
+++ include/header.php
@@ -1,9 +1,5 @@
 <nav>
-<<<<<<< HEAD
     <div class="logo-cont">
-        <img src="<?= $page != "accueil" ? "." : "" ?>./assets/img/logo.png" alt="logo de c.co.gestion">
+        <img src="<?= $page != "accueil" ? "." : "" ?>./assets/img/logo.png" alt="logo socknnect">
     </div>
-=======
-    <img src="../assets/img/logo-socknnect.png" alt="logo socknnect">
->>>>>>> 610b04977c20d20e624cf51f2459474e021bbb4f
 </nav>

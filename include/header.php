033fa2b8f6c7b1208a4f7deb556d9e636b961486
Alistair_Piot <alistair.pro15@gmail.com>
2024-09-10 15:54:36 +0200

    ajout page de base

---
+++ include/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <nav>
+        <img src="../assets/img/logo-socknnect.png" alt="logo socknnect">
+    </nav>
+</body>
+</html>

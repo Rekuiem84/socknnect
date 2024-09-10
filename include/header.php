610b04977c20d20e624cf51f2459474e021bbb4f
Alistair_Piot <alistair.pro15@gmail.com>
2024-09-10 16:30:02 +0200

    modif header.php

--- include/header.php
+++ include/header.php
@@ -1,13 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <nav>
-        <img src="../assets/img/logo-socknnect.png" alt="logo socknnect">
-    </nav>
-</body>
-</html>
+<nav>
+    <img src="../assets/img/logo-socknnect.png" alt="logo socknnect">
+</nav>

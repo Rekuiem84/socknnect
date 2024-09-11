335528517735aa948b4e03b942516f365a3589ff
Rekuiem84 <morinijoshua@gmail.com>
2024-09-11 10:03:50 +0200

    login et style

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
   <meta charset='UTF-8'>
   <meta http-equiv='X-UA-Compatible' content='IE=edge'>
   <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-  <title>Document</title>
+  <title>Socknnect</title>
   <meta name='description' content=''>
   <link rel='stylesheet' href='./assets/style/style.css' />
   <link rel='shortcut icon' type='image/png' href='' />
@@ -14,12 +14,12 @@
 
 <body>
   <?php
-  include "./include/header.php"
-  ?>
-  <main></main>
-  <?php
-  include "./include/footer.php"
-  ?>
+  $page = "accueil";
+  include "./include/header.php" ?>
+  <main>
+
+  </main>
+  <?php include "./include/footer.php" ?>
 </body>
 
 </html>

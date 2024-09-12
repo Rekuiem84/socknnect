44f09536d5f017cc1543b7ea666d3150ef6ebca8
powellvl <lucax.mb03@gmail.com>
2024-09-12 10:24:01 +0200

    Front Profil

--- pages/profil.php
+++ pages/profil.php
@@ -2,10 +2,9 @@
 session_start();
 
 // Vérifier si l'utilisateur est connecté
-if (!isset($_SESSION['id'])) {
+if (!isset($_SESSION['is_connected'])) {
     // Rediriger vers la page de connexion si l'utilisateur n'est pas connecté
-    // header('Location: login.php');
-    exit();
+    include "./redirect-index.php";
 }
 
 // Inclure les classes nécessaires (supposons que User et Db sont dans des fichiers séparés)
@@ -27,8 +26,53 @@ $user->getUser($_SESSION['id']); // Récupérer les informations de l'utilisateu
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Profil de l'utilisateur</title>
+    <link rel='stylesheet' href='../assets/style/style.css' />
 </head>
 <body>
+<?php
+$page = "profil";
+include "../include/header.php" ?>
+ <div class="bg-accent-1 window form-cont">
+      <h1>INSCRIPTION</h1>
+      <form method="post">
+        <div class="input-cont">
+          <label for="nom">Pseudo</label>
+          <input id="nom" value="<?= ($_SESSION['nom']); ?>"/>
+        </div>
+        <div class="input-cont">
+          <label for="couleur">Couleur</label>
+          <input id="couleur" value="<?= ($_SESSION['couleur']); ?>"/>
+        </div>
+        <div class="input-cont">
+          <label for="taille">Taille</label>
+          <input id="taille" value="<?= ($_SESSION['taille']); ?>"/>
+        </div>
+        <div class="input-cont">
+          <label for="matiere">Matière</label>
+          <input id="matiere" value="<?= ($_SESSION['matiere']); ?>"/>
+        </div>
+        <div class="input-cont">
+          <label for="motif">Motif</label>
+          <input id="motif" value="<?= ($_SESSION['motif']); ?>"/>
+        </div>
+        <div class="input-cont">
+          <label for="email">Adresse email</label>
+          <input id="email" value="<?= ($_SESSION['email']); ?>"/>
+        </div>
+        <div class="input-cont">
+          <label class="label-photo" for="photo">Télécharger une photo</label>
+          <div class="custom-file">
+            <input type="file" id="photo" name="photo" accept="image/*" required>
+            <label for="photo" class="file-label">Choisir une photo</label>
+          </div>
+        </div>
+        <!-- Pour l'aperçu de l'image -->
+        <div class="image-preview">
+          <img src="" alt="Prévisualisation de l'image" id="imagePreview">
+        </div>
+        <button class="btn-submit" type="submit">Modifier mon profil</button>
+      </form>
+    </div>
 <form method="post">
 <h2>Profil de <h2> <input id="nom" value="<?= ($_SESSION['nom']); ?>"/>
 

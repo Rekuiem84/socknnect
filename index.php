cc29531708c70ac259d713693517e297810103d4
Alistair_Piot <alistair.pro15@gmail.com>
2024-09-13 10:26:16 +0200

    modif profil photo : OK + ajout balise meta description

--- index.php
+++ index.php
@@ -11,6 +11,7 @@ require "./assets/classes/Login.php";
   <meta charset='UTF-8'>
   <meta http-equiv='X-UA-Compatible' content='IE=edge'>
   <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+  <meta name="description" content="Site de rencontre entre chaussettes" />
   <title>Socknnect</title>
   <meta name='description' content=''>
   <link rel='stylesheet' href='./assets/style/style.css' />
--- pages/welcome.php
+++ pages/welcome.php
@@ -39,7 +39,7 @@ if ($_SESSION["is_connected"]) :
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
     <meta name='viewport' content='width=device-width, initial-scale=1.0'>
     <title>Match</title>
-    <meta name='description' content=''>
+    <meta name="description" content="Site de rencontre entre chaussettes" />
     <link rel='stylesheet' href='../assets/style/style.css' />
     <link rel='shortcut icon' type='image/png' href='' />
     <script src='' defer></script>
